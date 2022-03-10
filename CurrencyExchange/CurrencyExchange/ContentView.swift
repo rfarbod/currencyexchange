2645754bfb134bc387d71055343081551cf5f2f8
Farbod Rahiminik <farbodrahiminik@Farbods-MacBook-Pro.local>
2022-03-10 19:01:29 +0330

    firtst commit + added required packages

---
+++ CurrencyExchange/CurrencyExchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyExchange
+//
+//  Created by Farbod Rahiminik on 3/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurrencyExchange/CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by Farbod Rahiminik on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
